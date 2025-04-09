5259a9a7b584df769338ddbab87a2c3e64658c07
lshdainty <lshdainty@gmail.com>
2025-04-09 12:58:17 +0900

    vite 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
-import * as path from 'path';
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgrPlugin from 'vite-plugin-svgr';
+import * as path from 'path';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgrPlugin()],
+  plugins: [react(), svgr()],
   server: {
     port: 3000,
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    }
-  }
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 10000,
+    target: 'es2020',
+  },
+  base: '/web'
 })
