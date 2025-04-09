af58273d9779803d5c42520f71865ab7a869ad49
lshdainty <lshdainty@gmail.com>
2025-04-09 17:59:51 +0900

    commit

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,28 +1,105 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Sidebar, Menu, MenuItem, SubMenu, menuClasses, MenuItemStyles } from 'react-pro-sidebar';
+import { Calendar } from '@/components/test/icons/Calendar';
 import '@/components/nav/nav.scss';
 
+type Theme = 'light' | 'dark';
+
+const themes = {
+  light: {
+    sidebar: {
+      backgroundColor: '#ffffff',
+      color: '#607489',
+    },
+    menu: {
+      menuContent: '#fbfcfd',
+      icon: '#0098e5',
+      hover: {
+        backgroundColor: '#c5e4ff',
+        color: '#44596e',
+      },
+      disabled: {
+        color: '#9fb6cf',
+      },
+    },
+  },
+  dark: {
+    sidebar: {
+      backgroundColor: '#0b2948',
+      color: '#8ba1b7',
+    },
+    menu: {
+      menuContent: '#082440',
+      icon: '#59d0ff',
+      hover: {
+        backgroundColor: '#00458b',
+        color: '#b6c8d9',
+      },
+      disabled: {
+        color: '#3e5e7e',
+      },
+    },
+  },
+};
+
+// hex to rgba converter
+const hexToRgba = (hex: string, alpha: number) => {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
+
 const Nav = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const [hasImage, setHasImage] = useState(false);
+  const [theme, setTheme] = useState<Theme>('light');
+
+  const menuItemStyles: MenuItemStyles = {
+    root: {
+      fontSize: '13px',
+      fontWeight: 400,
+    },
+    icon: {
+      color: themes[theme].menu.icon,
+      [`&.${menuClasses.disabled}`]: {
+        color: themes[theme].menu.disabled.color,
+      },
+    },
+    SubMenuExpandIcon: {
+      color: '#b6b7b9',
+    },
+    subMenuContent: ({ level }) => ({
+      backgroundColor:
+        level === 0
+          ? hexToRgba(themes[theme].menu.menuContent, hasImage && !collapsed ? 0.4 : 1)
+          : 'transparent',
+    }),
+    button: {
+      [`&.${menuClasses.disabled}`]: {
+        color: themes[theme].menu.disabled.color,
+      },
+      '&:hover': {
+        backgroundColor: hexToRgba(themes[theme].menu.hover.backgroundColor, hasImage ? 0.8 : 1),
+        color: themes[theme].menu.hover.color,
+      },
+    },
+    label: ({ open }) => ({
+      fontWeight: open ? 600 : undefined,
+    }),
+  }
+
   return (
-    <div className='nav'>
-      <nav className='nav-bar'>
-        <div className='nav-bar-logo'>
-          <div></div>
-        </div>
-        <div className='nav-bar-content'>
-          <ul className='nav-bar-menu'>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/work">Work</Link></li>
-            <li><Link to="/culture">Culture</Link></li>
-            <li><Link to="/rule">Rule</Link></li>
-          </ul>
-          <div className='nav-bar-user'>
-            <div className='nav-user-login'><Link to='/login'>Login</Link></div>
-            <div className='nav-user'></div>
-            <ul className='user-down'></ul>
-          </div>
-        </div>
-      </nav>
-    </div>
+    <Sidebar collapsed={true}>
+      <Menu menuItemStyles={menuItemStyles} closeOnClick={true}>
+        <MenuItem icon={<Calendar />}>Home</MenuItem>
+        <MenuItem icon={<Calendar />}>Work</MenuItem>
+        <MenuItem icon={<Calendar />}>Culture</MenuItem>
+        <MenuItem icon={<Calendar />}>Rule</MenuItem>
+      </Menu>
+    </Sidebar>
   );
 }
 
