2e1e64403132b42b93ac1fa5f6b9ecabd823365c
lshdainty <lshdainty@gmail.com>
2025-04-25 17:28:51 +0900

    markdown 적용 및 rule 페이지 변경

--- src/view/rule/Rule.tsx
+++ src/view/rule/Rule.tsx
@@ -1,28 +1,30 @@
+import { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import rehypeHighlight from "rehype-highlight";
 import "highlight.js/styles/a11y-dark.css";
 
 import '@/view/rule/rule.scss';
-import { useEffect } from "react";
 
-import { useState } from "react";
 const Rule: React.FC = () => {
   const [markdown, setMarkdown] = useState("");
 
   useEffect(() => {
-    fetch("./test.md")
+    fetch("./dept.md")
       .then((response) => response.text())
       .then((text) => setMarkdown(text));
   }, []);
 
   return (
     <div className='rule'>
-      <h1>rule page</h1>
-      <ReactMarkdown
-        remarkPlugins={[remarkGfm]}
-      >
-        {markdown}
-      </ReactMarkdown>
+      <div className='content'>
+        <ReactMarkdown
+          remarkPlugins={[remarkGfm]}
+          rehypePlugins={[rehypeHighlight]}
+        >
+          {markdown}
+        </ReactMarkdown>
+      </div>
     </div>
   );
 }
