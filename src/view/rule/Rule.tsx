7423989acca34d902a4b7e8e34cead97905f3ecb
lshdainty <lshdainty@gmail.com>
2025-04-25 17:01:19 +0900

    markdown 적용 및 파일 읽어서 표출

--- src/view/rule/Rule.tsx
+++ src/view/rule/Rule.tsx
@@ -1,9 +1,28 @@
-import '@/view/rule/rule.scss';
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import "highlight.js/styles/a11y-dark.css";
 
+import '@/view/rule/rule.scss';
+import { useEffect } from "react";
+
+import { useState } from "react";
 const Rule: React.FC = () => {
+  const [markdown, setMarkdown] = useState("");
+
+  useEffect(() => {
+    fetch("./test.md")
+      .then((response) => response.text())
+      .then((text) => setMarkdown(text));
+  }, []);
+
   return (
     <div className='rule'>
       <h1>rule page</h1>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+      >
+        {markdown}
+      </ReactMarkdown>
     </div>
   );
 }
