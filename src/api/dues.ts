bee4dadb15ea504b9ede229ed24326dfbb3d20e6
lshdainty <lshdainty@gmail.com>
2025-07-31 13:34:03 +0900

    사용자별 월별 생일 회비 입금내역 api 연동

--- src/api/dues.ts
+++ src/api/dues.ts
@@ -9,7 +9,9 @@ interface ApiResponse<T = any> {
 }
 
 const enum DuesQueryKey {
-  GET_YEAR_OPERATION_DUES = 'getYearOperationDues'
+  GET_YEAR_OPERATION_DUES = 'getYearOperationDues',
+  GET_MONTH_BIRTH_DUES = 'getMonthBirthDues',
+  GET_USERS_MONTH_BIRTH_DUES = 'getUsersMonthBirthDues'
 }
 
 interface getYearOperationDuesReq {
@@ -22,13 +24,63 @@ interface getYearOperationDuesResp {
   total_withdrawal: number
 }
 
-const useGetYearOperationDues = (d: getYearOperationDuesReq) => {
+const useGetYearOperationDues = (reqData: getYearOperationDuesReq) => {
   return useQuery({
-    queryKey: [DuesQueryKey.GET_YEAR_OPERATION_DUES, d.year],
+    queryKey: [DuesQueryKey.GET_YEAR_OPERATION_DUES, reqData],
     queryFn: async (): Promise<getYearOperationDuesResp> => {
       const resp: ApiResponse = await api.request({
         method: 'get',
-        url: `/dues/operation?year=${d.year}`
+        url: `/dues/operation?year=${reqData.year}`
+      });
+
+      if (resp.code !== 200) throw new Error(resp.data.data.message);
+
+      return resp.data;
+    }
+  });
+}
+
+interface useGetMonthBirthDuesReq {
+  year: string
+  month: string
+}
+
+interface useGetMonthBirthDuesResp {
+  birth_month_dues: number
+}
+
+const useGetMonthBirthDues = (reqData: useGetMonthBirthDuesReq) => {
+  return useQuery({
+    queryKey: [DuesQueryKey.GET_MONTH_BIRTH_DUES, reqData],
+    queryFn: async (): Promise<useGetMonthBirthDuesResp> => {
+      const resp: ApiResponse = await api.request({
+        method: 'get',
+        url: `/dues/birth/month?year=${reqData.year}&month=${reqData.month}`
+      });
+
+      if (resp.code !== 200) throw new Error(resp.data.data.message);
+
+      return resp.data;
+    }
+  });
+}
+
+interface useGetUsersMonthBirthDuesReq {
+  year: string
+}
+
+interface useGetUsersMonthBirthDuesResp {
+  dues_user_name: string
+  month_birth_dues: Array<number>
+}
+
+const useGetUsersMonthBirthDues = (reqData: useGetUsersMonthBirthDuesReq) => {
+  return useQuery({
+    queryKey: [DuesQueryKey.GET_USERS_MONTH_BIRTH_DUES, reqData],
+    queryFn: async (): Promise<useGetUsersMonthBirthDuesResp[]> => {
+      const resp: ApiResponse = await api.request({
+        method: 'get',
+        url: `/dues/users/birth/month?year=${reqData.year}`
       });
 
       if (resp.code !== 200) throw new Error(resp.data.data.message);
@@ -43,5 +95,7 @@ export {
   DuesQueryKey,
 
   // API Hook
-  useGetYearOperationDues
+  useGetYearOperationDues,
+  useGetMonthBirthDues,
+  useGetUsersMonthBirthDues
 }
